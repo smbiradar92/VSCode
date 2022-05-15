43ec2ce5c783a02fb90af8993cb6a175d588efd1
Smbiradar92 <smbiradar92@gmail.com>
2022-05-15 23:25:27 +0530

    Adding VSCode projects

---
+++ JS_Node/EmployeeWage.js
@@ -0,0 +1,9 @@
+const PRESENT = 1;
+
+let isPresent = Math.floor(Math.random()*2);
+
+if(isPresent == PRESENT){
+    console.log("Employee is Present");
+}else{
+    console.log("Employee is Absent");
+}
---
+++ JS_Node/arrayExample.js
@@ -0,0 +1,7 @@
+let origDogs = ["BullDog","Beagle","Labrdor"];
+let cats = new Array("Americal curl","Bengal");
+let birds = ["Falcons", "Ducks","flamingoes"];
+
+// Array copy Elements
+let sliceDogs = origDogs.slice(1,2);
+console.log(sliceDogs);
